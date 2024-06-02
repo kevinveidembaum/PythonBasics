99b4d33caeaed2cb10a4f4ddd6c040d9cb5fa231
Kevin Nikolai <117945004+kevinveidembaum@users.noreply.github.com>
2024-06-02 16:20:47 -0300

    using while for data validation

---
+++ gender.py
@@ -0,0 +1,12 @@
+def main():
+    genero = str(input("Digite seu sexo: [M][F]")).strip()[0]
+    while genero not in 'MmFf':
+        genero = str(input("Dados invalidos. Por favor informe seu sexo: ")).strip()[0]
+
+    if genero in 'Ff':
+        print("Obrigado, sexo Feminino confirmado com sucesso!")
+    else:
+        print("Obrigado, sexo Masculino confirmado com sucesso!")
+
+if __name__ == "__main__":
+    main()
